0d03fcdec1bb545a9be97cbea6ee437f4aa2cc93
Aser97 <lompoaser@yahoo.fr>
2022-05-19 14:35:56 +0200

    Initial Commit

---
+++ Chess/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Chess
+//
+//  Created by Aser on 19/05/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
